6cc00cd1724eee6f7cd8f08dd3e2bd5f272edebe
pablopenrose <kaderarnold@icloud.com>
2021-05-30 12:30:04 -0400

    got my SDL wrapper up and running

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import pygame
 
 def main ():
-    print ('success')
+    pygame.init()
+    logo = pygame.image.load("logo.png")
+    pygame.display.set_icon(logo)
+    pygame.display.set_caption("chess game")
+    screen = pygame.display.set_mode((512, 512))
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+    
 
 
 if __name__ == '__main__':
